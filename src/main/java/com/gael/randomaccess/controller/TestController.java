c727a3cb1b245fe2f555da7a50f6eec0216dbfab
GALL965 <bonaisdecamote45@gmail.com>
2025-06-14 17:27:27 -0600

    s

---
+++ src/main/java/com/gael/randomaccess/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.gael.randomaccess.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "OK TEST CORS";
+    }
+}
