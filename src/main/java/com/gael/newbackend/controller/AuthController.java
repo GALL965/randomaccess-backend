60ca84d166faaeaf38946eadef50ca61a68e6bbd
GALL965 <bonaisdecamote45@gmail.com>
2025-06-15 11:56:07 -0600

    feat: AuthController funcional y CORS para /api/auth/register

---
+++ src/main/java/com/gael/newbackend/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.gael.newbackend.controller;
+
+import com.gael.newbackend.model.User;
+import com.gael.newbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200") // CORS local aquí también
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity.badRequest().body("Email ya registrado");
+        }
+
+        userRepository.save(user);
+        return ResponseEntity.ok("Usuario registrado");
+    }
+}
