6e5e2871009b1b73f3aa7c82b4d64103f33fdaad
GALL965 <bonaisdecamote45@gmail.com>
2025-06-14 22:51:43 -0600

    fix: AuthenticationManager declarado como bean

--- src/main/java/com/gael/randomaccess/security/WebSecurityConfig.java
+++ src/main/java/com/gael/randomaccess/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.gael.randomaccess.security;
 
+import com.gael.randomaccess.security.jwt.JwtFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
