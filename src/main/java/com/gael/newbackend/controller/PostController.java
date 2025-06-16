d25984d8b5f9ea65a9da1e9af60f541112a6caaa
GALL965 <bonaisdecamote45@gmail.com>
2025-06-16 15:48:06 -0600

    w

--- src/main/java/com/gael/newbackend/controller/PostController.java
+++ src/main/java/com/gael/newbackend/controller/PostController.java
@@ -24,7 +24,7 @@ public class PostController {
 
 @PostMapping("/")
 public Post createPost(@RequestBody Map<String, String> data) {
-    User user = userRepository.findByUsername(data.get("username")).orElseThrow();
+User user = userRepository.findByEmail(data.get("username")).orElseThrow();
 
     Post post = new Post();
     post.setTitle(data.get("title"));
